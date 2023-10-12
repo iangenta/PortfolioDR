70ea6a329e246d2059b41192e08f3b4d4121993c
ian genta <gentaian2@gmail.com>
2023-10-12 17:25:43 -0300

    Projectlist initialize with an empty array

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -1,20 +1,24 @@
 import { ProjectCard } from './ProjectCard'
 import { useEffect, useState } from "react"
 import { getAllProjects } from "../api/project.api"
-export function ProjectList(){
-    const [projects, setProjects]  = useState()
-    useEffect(()=>{
-       async function loadProjects(){
-            const res = await getAllProjects();
-            setProjects(res.data);
-        }
-        loadProjects()
-    },[])
-    return(<div>
-        {projects.map(project=>(
-            <ProjectCard key={project.id} project={project}/>
+
+export function ProjectList() {
+  const [projects, setProjects] = useState([]); // Initialize with an empty array
+
+  useEffect(() => {
+    async function loadProjects() {
+      const res = await getAllProjects();
+      setProjects(res.data);
+    }
+
+    loadProjects();
+  }, []);
+
+  return (
+    <div>
+      {projects.map(project => (
+        <ProjectCard key={project.id} project={project} />
       ))}
     </div>
-      
-    )
-  }
+  );
+}
