c5c495eff6972ec3687cf12635c41ecf765ec0b1
ian genta <gentaian2@gmail.com>
2023-10-12 18:52:58 -0300

    Create projects

--- client/src/api/project.api.js
+++ client/src/api/project.api.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
-export const getAllProjects =()=>{
-    return axios.get('http://127.0.0.1:8000/projects/api/v1/projects/')
 
+//create instance 
+const projectApi = axios.create({
+    baseURL:'http://127.0.0.1:8000/projects/api/v1/projects/'
+})
+export const getAllProjects =()=>{
+    return projectApi.get('/')
+
+}
+export const createProject =(project)=>{
+    return projectApi.post('/',project)
 }
--- client/src/pages/ProjectFormPage.jsx
+++ client/src/pages/ProjectFormPage.jsx
@@ -1,6 +1,19 @@
+import {useForm} from 'react-hook-form'
+import { createProject } from '../api/project.api'
+import {useNavigate} from 'react-router-dom'
 export function ProjectFormPage(){
-    return(
-      <div>Project Form Page</div>
-    )
+  const {register,handleSubmit,formState:{errors},} = useForm()
+  const navigate = useNavigate()
+
+  return(
+      <form onSubmit={handleSubmit(async data=>{ 
+      await createProject(data);
+      navigate('/projects')})}>
+        <input type="text" placeholder="Title" {...register("title",{required : true})} />{errors.title && <span>this field is requiered</span>}
+        <textarea placeholder="Description" rows="3"  {...register("description",{required : true})}></textarea>{errors.description && <span>this field is requiered</span>}
+        <button>Save</button>
+      </form>
+
+      )
   }
   
